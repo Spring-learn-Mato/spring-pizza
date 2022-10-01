3633e7ff4fe0f967a5109d7569e2707f3a9b3bba
iqpizza6349 <iqpizza6349@gmail.com>
2022-10-01 15:08:41 +0900

    :tada: :: initial commit

---
+++ src/test/java/me/iqpizza6349/springpizza/SpringPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iqpizza6349.springpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
